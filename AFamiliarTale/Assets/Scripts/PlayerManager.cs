e7a60a5f325ab879281fd399bc541bce642db36c
pr0ldan <paulina699@icloud.com>
2023-11-05 00:08:23 -0500

    coin system

--- AFamiliarTale/Assets/Scripts/PlayerManager.cs
+++ AFamiliarTale/Assets/Scripts/PlayerManager.cs
@@ -13,7 +13,11 @@ public class PlayerManager : MonoBehaviour
     public static bool isLevelComplete;
     public static Vector2 lastCheckpointPos = new Vector2(-10.5f, -4.993932f);
 
+    public static int numberOfCoins;
+    public TextMeshProUGUI coinsText;
+
     private void Awake() {
+        numberOfCoins = PlayerPrefs.GetInt("NumberOfCoins", 0);
         isGameOver = false;
         GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckpointPos;
     }
@@ -33,6 +37,9 @@ public class PlayerManager : MonoBehaviour
     }
 
     void Update() {
+        //Debug.Log(numberOfCoins);
+        coinsText.text = numberOfCoins.ToString();
+
         if(isGameOver) {
             gameOverScreen.SetActive(true);
         }
