b42ecc75b98f24628aa41878c78665f017673c2f
pr0ldan <paulina699@icloud.com>
2023-12-07 01:26:21 -0600

    wizard & background on all levels

--- AFamiliarTale/Assets/Scripts/Coins.cs
+++ AFamiliarTale/Assets/Scripts/Coins.cs
@@ -7,7 +7,6 @@ public class Coin : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.transform.tag == "Player") {
             PlayerManager.numberOfCoins++;
-
             PlayerManager.CoinSound();
             Destroy(gameObject);
         }
